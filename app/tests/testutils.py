4bd70c911c32d2dff9cf684ae40efe12dff583a1
SwatiSuri <89620136+SwatiSuri@users.noreply.github.com>
2021-10-03 21:57:43 -0500

    Update testutils.py

--- app/tests/testutils.py
+++ app/tests/testutils.py
@@ -1,2 +1,16 @@
 # Create a unit test method to test the calculate_avg_rating function created in the utils module
 # Make sure you include multiple unit tests to test that the function acts as expected in different scenarios
+from app.src.Review import review
+from app.src.ratingscalc import cal_avg_rating
+import unittest as ut
+
+class test_rating(ut.TestCase):
+
+    def test_cal_avg_rating(self):
+        reviews = []
+        reviews.append(review('HappyPlace', 'Best Restaurant To Eat', 'Great', 5))
+        reviews.append(review('Pizzzaaa', 'Best Restaurant To Eat', 'Bad', 1))
+        reviews.append(review('sweetworld', 'Best Restaurant To Eat', 'ok', 2.5))
+        reviews.append(review('goodtimes', 'Best Restaurant To Eat', 'worst', 0))
+    result = cal_avg_rating(reviews)
+    self.assertEqual(0, result(rating))

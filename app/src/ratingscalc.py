370d114c5969c3c1dde68c5e95f4a4f4e4ebde9e
SwatiSuri <89620136+SwatiSuri@users.noreply.github.com>
2021-10-03 21:51:31 -0500

    Update ratingscalc.py

--- app/src/ratingscalc.py
+++ app/src/ratingscalc.py
@@ -6,3 +6,21 @@
 #
 # If the list of reviews is empty, return an empty dictionary
 # Make sure that you add type hints to the function paramter and return value
+from Review import review
+from utils import  cal_avg_rating
+
+def cal_avg_rating(reviews: list[review]) -> dict[int, float]:
+    restaurant_rating ={}
+    total_ratings = {}      
+    if reviews == none:
+       return {}
+    for review in reviews:
+        if review.getrestaurant() in restaurant_rating.keys():
+           restaurant_rating[review.getrestaurant()] += review.getrating()
+           total_ratings[review.getrestaurant()] += 0 
+        else:
+            restaurant_rating[review.getrestaurant()] = review.getrating()
+            total_ratings[review.getrestaurant()] = 0
+    for restaurant in restaurant_rating.keys():
+        restaurant_rating[restaurant] = round((restaurant_rating[restaurant])/total_ratings[restaurant],2)
+    return restaurant_rating
